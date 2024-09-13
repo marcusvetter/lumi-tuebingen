1bb14b5998b0d9721f7f35fa120fc93ac3703bc4
Marcus Vetter <marcus.vetter@live.com>
2024-09-13 22:07:05 +0200

    feat: first layout texts and colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,12 +8,14 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      colors: {
+        "lumi-white": "#FAFDF6",
+        "lumi-blue": "#A5D9DA",
+        "lumi-yellow": "#FBCB83",
+        "lumi-red": "#FCD1CB",
+        "lumi-green": "#BBE4C6",
       },
-    },
+    }
   },
   plugins: [],
 };

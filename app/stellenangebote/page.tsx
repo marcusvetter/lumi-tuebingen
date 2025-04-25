2b4dc3fc854cd4530c01753d8ad0adfc4b54627b
Marcus Vetter <marcus.vetter@live.com>
2025-04-25 21:11:25 +0200

    feat: add stellenangebote

---
+++ app/stellenangebote/page.tsx
@@ -0,0 +1,96 @@
+export default function Page() {
+    return <div>
+        <h2>Pädagogische Fachkraft</h2>
+
+        <p>Wir suchen pädagogische Fachkräfte (m/w/d) in Voll- und Teilzeit.</p>
+        <p>
+            <u>Du passt zu uns, wenn Du:</u>
+            <ul>
+                <li>staatlich anerkannte/r Erzieher/in (m/w/d) bist oder einen vergleichbaren Abschluss hast</li>
+                <li>Dir vorstellen kannst im Zeitfenster von 7:30 bis 15:00 Uhr in wöchentlich wechselnden Schichten zu arbeiten</li>
+                <li>Lust hast, Dich in ein Team zu integrieren und Deine Arbeit eigenverantwortlich zu gestalten</li>
+                <li>bereit bist, Eingewöhnungen durchzuführen, für und mit Deinen künftigen Bezugskindern Portfolios zu gestalten und Elterngespräche zu führen</li>
+                <li>Freude daran hast, altersentsprechende Aktivitäten zu planen und durchzuführen</li>
+                <li>bereit bist, Dich in regelmäßig stattfindenden Teamsitzungen aktiv einzubringen</li>
+                <li>Dich mit unserem Konzept und unseren Schwerpunkten identifizieren kannst</li>
+                <li>Donnerstags von 15.00Uhr bis 17.00Uhr Zeit für ein gemeinsames Team hast</li>
+            </ul>
+        </p>
+        <p>
+            <u>Wir bieten Dir:</u>
+            <ul>
+                <li>ein offenes, starkes und engagiertes Gesamtteam von 7 Personen</li>
+                <li>großen Gestaltungsspielraum der pädagogischen Arbeit und des Gruppenalltags</li>
+                <li>eigenverantwortliche Gestaltung der Verfügungszeit</li>
+                <li>Bezahlung in Anlehnung an den TVöD und eine betriebliche Altersvorsorge</li>
+                <li>Teamfortbildungen, pädagogische Tage und Weiterbildungsmöglichkeiten</li>
+            </ul>
+        </p>
+        <p>
+            Wenn wir Dein Interesse geweckt haben und Du Teil des LUMI-Teams werden
+            möchtest, freuen wir uns sehr auf Deine Bewerbung, gerne per Mail, an:
+        </p>
+        <p>
+            LUMI - Leben mit Kindern e.V.<br />
+            Nürtingerstraße 21/1<br />
+            72074 Tübingen
+        </p>
+        <p>
+            Telefon: 0172-9794482<br />
+            E-Mail: info at lumi-tuebingen punkt de
+        </p>
+
+        <h2>Aushilfskräfte</h2>
+        <p>
+            Wir suchen immer wieder Aushilfskräfte für unsere Einrichtung, die uns bei der Arbeit
+            mit den Kindern unterstützen.
+        </p>
+        <p>
+            <u>Du passt zu uns, wenn Du:</u>
+            <ul>
+                <li>Lust hast, mit Kindern von 1 bis 3 Jahren zu arbeiten</li>
+                <li>Freude daran hast, die pädagogischen Fachkräfte in ihrer Arbeit zu unterstützen</li>
+                <li>eine sinnvolle und bereichernde Tätigkeit suchst</li>
+                <li>Spontan, motiviert und flexibel bist</li>
+                <li>Gerne draußen bist und Lastenrad fahren kannst</li>
+            </ul>
+        </p>
+        <p>
+            <u>Wir bieten Dir:</u>
+            <ul>
+                <li>eine Aushilfstätigkeit auf Basis der steuerfreien Übungsleiterpauschale</li>
+                <li>Einsätze flexibel nach Absprache</li>
+                <li>viele Einblicke in den pädagogischen Alltag</li>
+            </ul>
+        </p>
+        <p>
+            Wenn wir Dein Interesse geweckt haben und Du Teil des LUMI-Teams werden
+            möchtest, freuen wir uns sehr auf Deine Bewerbung, gerne per Mail, an:
+        </p>
+        <p>
+            LUMI - Leben mit Kindern e.V.<br />
+            Nürtingerstraße 21/1<br />
+            72074 Tübingen
+        </p>
+        <p>
+            Telefon: 0172-9794482<br />
+            E-Mail: info at lumi-tuebingen punkt de
+        </p>
+
+        <h2>FSJ (freiwilliges soziales Jahr)</h2>
+        <p>
+            Wir freuen uns über Interessierte für ein FSJ in unserer Einrichtung.
+        </p>
+        <p>
+            Melde dich gerne direkt bei uns oder informiere dich direkt beim <a href="https://www.dachverband-tuebingen.de/" target="_blank">FSJ-Träger</a> des Dachverbands der kleinen freien Kita-Träger Tübingen e.V.
+        </p>
+
+        <h2>Praktikum</h2>
+        <p>
+            Auch Praktikantinnen und Praktikanten jeglicher Art begrüßen wir immer gerne im LUMI.
+        </p>
+        <p>
+            Frag einfach direkt bei uns an, ob wir derzeit bzw. für deinen benötigten Zeitraum Kapazitäten frei haben.
+        </p>
+    </div>
+}

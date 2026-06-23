449cac2f5b8fcf2331702b1a7d6b6017512b1cf1
Marcus Vetter <marcus.vetter@live.com>
2026-06-23 19:56:15 +0200

    fix: adjust width of stellenausschreibung for mobile devices

--- app/components/PdfViewer.tsx
+++ app/components/PdfViewer.tsx
@@ -4,10 +4,10 @@ interface PdfViewerProps {
 
 export default function PdfViewer({ pdfPath }: PdfViewerProps) {
   return (
-    <div className="mb-8">
+    <div className="mb-8 overflow-x-auto">
       <iframe
         src={`${pdfPath}#toolbar=0&nav=0&view=FitH`}
-        className="w-full aspect-[1/1.414] border-0"
+        className="w-full max-w-full aspect-[1/1.414] border-0"
         tabIndex={-1}
       />
     </div>
